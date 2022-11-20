12d61fee314a32fd38f4b6111dbcd11dd7b3119e
Sivasai Dharmateja <117574650+sivasai008@users.noreply.github.com>
2022-11-20 16:43:24 +0530

    Update shopping_mall_sql_queries.sql

--- shopping_mall_sql_queries.sql
+++ shopping_mall_sql_queries.sql
@@ -27,16 +27,17 @@ order by 2 desc;
 #top5_and_least_5_profit_City_wise
 
 (
-select city,sum(Sales) as total_Sales from  shopping.superstore
+select city,sum(Sales) as Total_Sales from  shopping.superstore
 group by 1
 order by 2 desc
 limit 5)
 UNION
 (
-select city,sum(Sales) as total_Sales from  shopping.superstore
+select city,sum(Sales) as Total_Sales from  shopping.superstore
 group by 1
 order by 2 asc
 limit 5)
+
 
 
 
